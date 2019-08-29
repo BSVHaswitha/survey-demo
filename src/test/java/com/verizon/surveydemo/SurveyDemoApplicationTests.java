a87b250acf6f0fee4fc17216b976ac0528d68d84
haswitha <bsvhaswitha@gmail.com>
2019-08-29 11:54:06 +0530

    test

---
+++ src/test/java/com/verizon/surveydemo/SurveyDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.verizon.surveydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SurveyDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
